14fd82555acb35cecbfa196db7e0e9661fcc76c2
NiBienvenu <nicibienvenu@gmail.com>
2024-08-30 16:18:21 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,46 @@
+var a = 5
+var b = 7
+const c = 10
+ //les types de variable en javascript
+ var d = "bonjour"
+ var e = true
+ var f = 12.5
+
+ console.log("type de variable d :"+typeof d)
+ console.log("type de variable e :"+typeof e)
+ console.log("type de variable f :"+typeof f)
+
+ // les constantes en javascript
+ console.log("constantes")
+ console.log("type de variable c :"+typeof c)
+ console.log(c)
+
+ // les operateurs arithmetiques
+ console.log("operateurs arithmetiques")
+ console.log(a+" + "+b +"="+(a+b))
+ console.log(a+" - "+b +"="+(a-b))
+ console.log(a+" * "+b +"="+(a*b))
+ console.log(a+" / "+b +"="+(a/b))
+ console.log(a+" % "+b +"="+(a%b))
+ console.log(a+" ** "+b +"="+(a**b))
+
+a = 11
+console.log(a+" + "+b +"="+(a+b))
+// les operateurs en javascript
+console.log(a+" - "+b +"="+(a-b))
+console.log(a+" * "+b +"="+(a*b))
+/*console.log(a+" / "+b +"="+(a/b))
+console.log(a+" % "+b +"="+(a%b))
+console.log(a+" ** "+b +"="+(a**b))*/
+
+
+// operateurs de comparaison
+console.log("operateur de comparaison")
+console.log(a+" > "+b +"="+(a>b))
+console.log(a+" < "+b +"="+(a<b))
+console.log(a+" >= "+b +"="+(a>=b))
+console.log(a+" <= "+b +"="+(a<=b))
+console.log(a+" == "+b +"="+(a==b))
+console.log(a+"!= "+b +"="+(a!=b))
+
+
